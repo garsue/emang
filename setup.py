6bfb9874473f3011a45c01605014688c69ef290d
Gar M. Sue <labs.garsue@gmail.com>
2012-08-30 05:57:19 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#vim: fileencoding=utf-8
+
+from setuptools import setup, find_packages
+setup(
+        name = "garsue_style",
+        version = "1.0",
+        packages = find_packages(),
+        )
