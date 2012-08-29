ef4c4d4c2e8b5c7e00b97428614d162bdf33249d
Gar M. Sue <labs.garsue@gmail.com>
2012-08-30 04:51:25 +0900

    start version control

---
+++ garsue_style.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#vim: fileencoding=utf-8
+
+import os
+from os import path
+import re
+from functools import partial, wraps
+from itertools import compress
+
+
+def get_matches(files):
+    # group(1): author, group(2): title, group(3): extension
+    pattern = re.compile(r"\[(.*)\](.*)\.(.*)")
+    return [re.match(pattern, n) for n in files]
+
+
+def get_old_filenames(files, matches):
+    return list(compress(files, matches))
+
+
+def get_filename_parts(match):
+    """Return (author, title, extension)"""
+    return match.group(1), match.group(2).rstrip("_2"), match.group(3)
+
+
+def compose_new_filenames(matches):
+    filename_parts = [get_filename_parts(m) for m in matches if m]
+    return ["{0} - {1}.{2}".format(a, t, e) for a, t, e in filename_parts]
+
+
+def list_filenames(olds, news):
+    if olds and news:
+        print("Rename to:")
+        [print("\t", old, "->", new) for old, new in zip(olds, news)]
+    print("Nothing to rename.")
+    exit()
+
+
+def require_confirm(f):
+    @wraps
+    def decorated(*args, **kwargs):
+        ans = input("Do you want to rename? ('yes' or 'no'): ")
+        if ans in ["y", "yes"]:
+            f(*args, **kwargs)
+            print("Done!")
+        print("Canceled by user.")
+
+
+@require_confirm
+def execute(curdir, olds, news):
+    abspath = partial(path.join, curdir)
+    [os.rename(abspath(old), abspath(new)) for old, new in zip(olds, news)]
+
+
+if __name__ == "__main__":
+    curdir = path.dirname(path.abspath(os.curdir))
+    files = next(os.walk(curdir))[2]
+    matches = get_matches(files)
+    olds = get_old_filenames(files, matches)
+    news = compose_new_filenames(matches)
+    list_filenames(olds, news)
+    execute(curdir, olds, news)
---
+++ test_garsue_style.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#vim: fileencoding=utf-8
+
+import unittest
+import os
+from os import path
+import re
+
+import renamer
+
+
+class TestRenamer(unittest.TestCase):
+    pattern = re.compile(r"\[(.*)\](.*)\.(.*)")
+    files = [
+            "spam1",
+            "spam2",
+            "[author]title.exp",
+            "[author]title_2.exp",
+            "[作者]タイトル.exp",
+            "[作者]タイトル_2.exp",
+            ]
+    matches = [
+            None,
+            None,
+            re.match(pattern, "[author]title.exp"),
+            re.match(pattern, "[author]title_2.exp"),
+            re.match(pattern, "[作者]タイトル.exp"),
+            re.match(pattern, "[作者]タイトル_2.exp"),
+            ]
+    olds = [
+            "[author]title.exp",
+            "[author]title_2.exp",
+            "[作者]タイトル.exp",
+            "[作者]タイトル_2.exp",
+            ]
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_current_dir(self):
+        test = path.abspath(os.curdir)
+        expect = path.dirname(path.abspath(__file__))
+        self.assertEqual(test, expect)
+
+    def test_get_matches(self):
+        test = renamer.get_matches(self.files)
+        self.assertIsNone(test[0])
+        self.assertIsNone(test[1])
+        self.assertEqual(test[2].groups(), ("author", "title", "exp"))
+        self.assertEqual(test[3].groups(), ("author", "title_2", "exp"))
+        self.assertEqual(test[4].groups(), ("作者", "タイトル", "exp"))
+        self.assertEqual(test[5].groups(), ("作者", "タイトル_2", "exp"))
+
+    def test_get_old_filenames(self):
+        test = renamer.get_old_filenames(self.files, self.matches)
+        self.assertEqual(test, self.olds)
+
+    def test_get_filename_parts(self):
+        pattern = re.compile(r"\[(.*)\](.*)\.(.*)")
+        filename = "[作者]タイトル_2.exp"
+        test = renamer.get_filename_parts(re.match(pattern, filename))
+        expect = "作者", "タイトル", "exp"
+        self.assertEqual(test, expect)
+
+    def test_compose_new_filenames(self):
+        test = renamer.compose_new_filenames(self.matches)
+        expect = [
+                "author - title.exp",
+                "author - title.exp",
+                "作者 - タイトル.exp",
+                "作者 - タイトル.exp",
+                ]
+        self.assertEqual(test, expect)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
