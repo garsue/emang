fdbe1ab92458215f2105e616352de52f15318eea
Gar Sue <labs.garsue@gmail.com>
2012-11-25 14:49:48 +0900

    switch builtin module name by major version

--- test/test_main.py
+++ test/test_main.py
@@ -6,11 +6,13 @@ import unittest
 
 from mock import patch
 from emang import main
+from sys import version_info
+builtin_module_name = "__builtin__" if version_info.major < 3 else "builtins"
 
 
 class TestMain(unittest.TestCase):
     def test_invalid_command(self):
-        with patch("__builtin__.print") as m:
+        with patch(builtin_module_name + ".print") as m:
             main.invalid_command()
             args, _ = m.call_args
             self.assertEqual(args[0], "No such a command")
