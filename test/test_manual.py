34166dc28096fe0afee9dd39be02f9660f01ba4f
Gar Sue <labs.garsue@gmail.com>
2012-11-25 21:03:27 +0900

    add test_to_filename_tuples

--- test/test_manual.py
+++ test/test_manual.py
@@ -3,7 +3,10 @@
 
 from __future__ import print_function, unicode_literals
 import unittest
+import site
+import io
 
+from mock import patch
 from emang import manual
 
 
@@ -14,15 +17,7 @@ class TestManual(unittest.TestCase):
         "[author]title.exp",
         "[作者]タイトル.exp"]
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_build_tempfile_body(self):
-        test = manual.build_tempfile_body(self.files)
-        expect = """old: spam1
+    tempfile_body = """old: spam1
 new: spam1
 
 old: spam2
@@ -33,10 +28,7 @@ new: [author]title.exp
 
 old: [作者]タイトル.exp
 new: [作者]タイトル.exp"""
-        self.assertEqual(test, expect)
-
-    def test_to_tuples(self):
-        rename_table = """old: spam1
+    rename_table = """old: spam1
 new: spam1
 
 old: spam2
@@ -47,8 +39,33 @@ new: author - title.exp
 
 old: [作者]タイトル.exp
 new: 作者 - タイトル.exp"""
-        test = manual.to_tuples(rename_table)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_build_tempfile_body(self):
+        test = manual.build_tempfile_body(self.files)
+        self.assertEqual(test, self.tempfile_body)
+
+    def test_to_tuples(self):
+        test = manual.to_tuples(self.rename_table)
         expect = [
             ("[author]title.exp", "author - title.exp"),
             ("[作者]タイトル.exp", "作者 - タイトル.exp")]
         self.assertEqual(test, expect)
+
+    def test_to_filename_tuples(self):
+        with patch("subprocess.call"):
+            test = manual.to_filename_tuples(self.files)
+            self.assertEqual(test, [])
+        open_name = site.builtins.__name__ + ".open"
+        config = {"return_value": io.BytesIO(self.rename_table.encode("utf8"))}
+        with patch("subprocess.call"), patch(open_name, **config):
+            test = manual.to_filename_tuples(self.files)
+            expect = [
+                ("[author]title.exp", "author - title.exp"),
+                ("[作者]タイトル.exp", "作者 - タイトル.exp")]
+            self.assertEqual(test, expect)
