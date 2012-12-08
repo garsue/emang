f267aeaee334728d36364cad9a3fef81713e02d2
Gar Sue <labs.garsue@gmail.com>
2012-12-09 02:14:31 +0900

    add test_main in test_autorename

--- test/test_autorename.py
+++ test/test_autorename.py
@@ -5,6 +5,7 @@ from __future__ import print_function, unicode_literals
 import unittest
 import re
 
+from mock import patch
 from emang import autorename
 
 
@@ -58,3 +59,20 @@ class TestAutoRename(unittest.TestCase):
     def test_compose_new_filenames(self):
         test = autorename.compose_new_filenames(self.matches)
         self.assertEqual(test, self.news)
+
+    def test_main(self):
+        filename_tuples = [
+            ("[author]title.exp", "author - title.exp"),
+            ("[author]title_2.exp", "author - title.exp"),
+            ("[作者]タイトル.exp", "作者 - タイトル.exp"),
+            ("[作者]タイトル_2.exp", "作者 - タイトル.exp")]
+        attrs = {
+            "get_files.return_value": self.files,
+            "list_up.return_value": filename_tuples,
+            "check_new_existence.return_value": filename_tuples,
+            "require_confirm.return_value": filename_tuples,
+            "execute_rename.return_value": filename_tuples,
+            "done.return_value": filename_tuples}
+        with patch("emang.autorename.common", **attrs):
+            test = autorename.main()
+            self.assertEqual(test, filename_tuples)
